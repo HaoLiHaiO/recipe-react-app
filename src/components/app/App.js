09e79ed1fda0a19cdfcef35c8c76bcc5a1bd8f70
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-20 12:54:59 +0300

    add app

---
+++ src/components/app/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Navbar from "../Navbar";
+import { Bodygrid } from "../Bodygrid";
+import { Footer } from "../Footer";
+
+function App() {
+  return (
+    <div>
+      <h1>
+        LESRECE<span className="highlight">TT</span>ESDALEX.COM
+      </h1>
+      <div className="App">
+        {/* navbar */}
+        <Navbar />
+
+        {/* Bodygrid */}
+        {/* Footer */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
